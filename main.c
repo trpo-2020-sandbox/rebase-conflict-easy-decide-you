7c82b85f9734a997c1475d2ee7d963ee395523df
srwxrwxrwx <srwxrwxrwx@users.noreply.github.com>
2017-03-04 18:19:47 +0700

    Separate elements with comma

--- main.c
+++ main.c
@@ -21,9 +21,11 @@ int main()
     int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
     const int min = min_element(arr, N_ELEMENTS(arr));
     printf("Array: ");
-    int i;
-    for (i = 0; i < N_ELEMENTS(arr); ++i) {
-        printf("%d ", arr[i]);
+    int i = 0;
+    printf("%d", arr[i]);
+    ++i;
+    for (; i < N_ELEMENTS(arr); ++i) {
+        printf(", %d", arr[i]);
     }
     printf("\n");
 
